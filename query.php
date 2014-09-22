f5c55ba81769079bdf6bd22f852d12aa154ffda5
joyeusenoelle <noelle.d.anthony@gmail.com>
2014-09-22 19:33:01 -0400

    9/22 Commit A

--- query.php
+++ query.php
@@ -32,10 +32,10 @@
 		}
 	};
 	function formval() {
-		var qname = $.trim($('#query_name').val());
-		var qmail = $.trim($('#query_email').val());
-		var qtext = $.trim($('#query_text').val());
-		var qtype = $('#query_type').val();
+		var qname = $.trim($('#query-name').val());
+		var qmail = $.trim($('#query-email').val());
+		var qtext = $.trim($('#query-text').val());
+		var qtype = $('#query-type').val();
 		var errorstr = "";
 		var haserrors = false;
 		var re = /^[A-Z0-9._%+!#-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
@@ -64,18 +64,18 @@
 		$("#cursor").toggle();
 	},500));
 	$(document).ready(function() {
-	});
-	$("#ndaquery").on("submit", function() {
-		alert("Clicked");
-		event.preventDefault;
-		if (formval()) {
-			/*var qname = $.trim($('#query_name').val());
-			var qmail = $.trim($('#query_email').val());
-			var qtext = $.trim($('#query_text').val());
-			var qtype = $('#query_type').val();*/
-			alert($('#ndaquery').serialize());
-		}
-		return false;
+		$("form#ndaquery").on("submit", function() {
+			//alert("Clicked");
+			event.preventDefault;
+			if (formval()) {
+				/*var qname = $.trim($('#query-name').val());
+				var qmail = $.trim($('#query-email').val());
+				var qtext = $.trim($('#query-text').val());
+				var qtype = $('#query-type').val();*/
+				alert($('#ndaquery').serialize());
+			}
+			return false;
+		});
 	});
 </script>
 </head>
@@ -85,14 +85,14 @@
 	<p>Name: <?php xecho($name); ?></p>
 	<p>Email: <?php xecho($email); ?></p>
 <?php } else { ?>
-	<h1>Post:</h1>
-	<form name="ndaquery" method="POST" action="">
+	<h1>Send a Work Query:</h1>
+	<form name="ndaquery" id="ndaquery" method="POST" action="">
 	<div class="form-row">
 		<div class="form-cell form-cell-left">
 			Your name:
 		</div>
 		<div class="form-cell form-cell-right">
-			<input type="text" name="query_name" size="40" value="">
+			<input type="text" name="query-name" size="40" value="">
 		</div>
 	</div>
 	<div class="form-row">
@@ -100,10 +100,41 @@
 			Your email address: 
 		</div>
 		<div class="form-cell form-cell-right">
-			<input type="text" name="query_email" size="40" value="">
+			<input type="text" name="query-email" size="40" value="">
 		</div>
-	</div><br>
-	<input type="submit" id="form-submit" style="clear: both;">
+	</div>
+    <div class="form-row">
+    	<div class="form-cell form-cell-left">
+        	Query type:
+        </div>
+        <div class="form-cell form-cell-right">
+        	<select id="query-type">
+            	<option value="WordPress Plugin">WordPress Plugin</option>
+                <option value="Software Setup">Web Software Setup</option>
+                <option value="Custom Web Software">Custom Web Software</option>
+                <option value="Retainer">Retainer</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-row">
+    	<div class="form-cell form-cell-left">
+        	Query description:<br>
+            Please let me know as many details as you can! If you've chosen a retainer, I'll help you decide what level is best for you based on your needs.
+        </div> 
+        <div class="form-cell form-cell-right">
+        	<textarea id="query-text" name="query-text" rows="10" cols="72">
+			</textarea>
+    	</div>
+    </div>    
+    <div class="form-row">
+    	<div class="form-cell form-cell-left">
+        	Sending this query is not a work agreement! If we agree to work together, I'll email you a contract.<br>
+            I do ask for a 50% deposit when the contract is signed.
+        </div>
+    	<div class="form-cell form-cell-right">
+			<input type="submit" id="form-submit" style="clear: both;" value="Send Query">
+        </div>
+    </div>
 	</form>
 <?php } ?>
 	
